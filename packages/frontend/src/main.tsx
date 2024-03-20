298b9faa3d2e4b9cb1c9644b1afe75ac830420f8
Edward Miller <edmiller.me@gmail.com>
2024-03-20 21:50:20 +1300

    Editing Amplify config

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -9,28 +9,25 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 Amplify.configure({
   Auth: {
-    //@ts-expect-error IDK
-    mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    Cognito: {
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolClientId: config.cognito.APP_CLIENT_ID,
+    },
   },
   Storage: {
-    //@ts-expect-error IDK
-    region: config.s3.REGION,
-    bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    S3: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+    },
   },
   API: {
-    //@ts-expect-error IDK
-    endpoints: [
-      {
-        name: "notes",
+    REST: {
+      YourAPIName: {
         endpoint: config.apiGateway.URL,
         region: config.apiGateway.REGION,
       },
-    ],
+    },
   },
 });
 
